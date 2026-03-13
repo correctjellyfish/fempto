19e919db73342cb7c34eb129d7f0aa5823c1c2c8
Braden Griebel <bgriebel@uw.edu>
2026-03-13 10:27:57 -0700

    feat(buffer): Added methods for splitting and manipulating rows

--- buffer.go
+++ buffer.go
@@ -4,12 +4,20 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"slices"
 )
+
+// Constant for whitespace
+var whitespace []rune = []rune{' ', '-', '_'}
 
 // Row represents a single row of text in a Buffer
 type Row struct {
-	idx     int
-	size    int
+	// The index of the row within the buffer,
+	// mostly used for accessing neighboring rows
+	idx int
+	// Length of the content in the Row
+	size int
+	// The text in the Row
 	content []rune
 }
 
@@ -18,11 +26,63 @@ type Row struct {
 func newRow(text string, rowIndex int) Row {
 	r := Row{
 		idx:     rowIndex,
-		size:    len(text),
 		content: []rune(text),
 	}
+	r.size = len(r.content)
 
 	return r
+}
+
+// Split a row starting at the given position, shortening the
+// calling row and returning the remainder of the line
+// as a new Row. This new row will share its idx with the
+// calling row, and so that needs to be updated.
+func (row *Row) splitRow(position int) Row {
+	// Get the content (this is possibly empty)
+	newContent := make([]rune, 0, row.size-position)                                         // Allocate slice that is large enough
+	newContent = slices.AppendSeq(newContent, slices.Values(row.content[position:row.size])) // Add the content
+	newRow := Row{idx: row.idx, size: len(newContent), content: newContent}                  // Create the new row
+
+	// Shorten the calling row
+	row.content = row.content[0:position]
+	row.size = len(row.content)
+
+	// Return the newly created row
+	return newRow
+}
+
+// Insert a character into the row at the provided position
+func (row *Row) insertChar(position int, char rune) {
+	// NOTE: Panics if the position is too large
+	row.content = slices.Insert(row.content, position, char)
+	row.size++
+}
+
+// Delete a character at the specified position in the row.
+//
+// If the position is too large, does nothing.
+func (row *Row) deleteChar(position int) {
+	if position >= row.size {
+		// Do nothing if position if too large
+		return
+	}
+	// Remove element
+	row.content = slices.Delete(row.content, position, position+1)
+	row.size--
+}
+
+// Return the position of the next word following the
+func (row *Row) nextWord(position int) int {
+	if position >= row.size {
+		return row.size - 1
+	}
+	curIdx := position
+	for ; curIdx < row.size; curIdx++ {
+		if slices.Contains(whitespace, row.content[curIdx]) {
+			break
+		}
+	}
+	return curIdx
 }
 
 // Buffer is a representation of lines of text, normally read from a file
---
+++ buffer_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestRowSplit(t *testing.T) {
+	testRow := newRow("Hello", 0)
+	testSplitRow := testRow.splitRow(4)
+	wantRemain := newRow("Hell", 0)
+	wantSplit := newRow("o", 0)
+	if !slices.Equal(testRow.content, wantRemain.content) || !slices.Equal(testSplitRow.content, wantSplit.content) {
+		t.Errorf(`SplitRow("Hello",4) = %q, %q, want match for %q, %q`, testRow.content, testSplitRow.content, wantRemain.content, wantSplit.content)
+	}
+}
