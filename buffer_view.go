a057514bb903fca7b903d2a504f52ed48c1b5be3
Braden Griebel <bgriebel@uw.edu>
2026-03-15 20:10:48 -0700

    feat(ui): Added struct for viewing a buffer

---
+++ buffer_view.go
@@ -0,0 +1,26 @@
+package main
+
+// A view of a buffer drawn to the terminal
+type BufferView struct {
+	// The buffer being viewed
+	buffer Buffer
+	// The cursor position within the buffer
+	cursor Position
+	// The position of the BufferView in the terminal
+	location Position
+	// The number of columns the BufferView occupies
+	width int
+	// The number of rows the BufferView occupies
+	height int
+}
+
+// Create a new BufferView from a buffer
+func newBufferView(buffer Buffer, location Position, width int, height int) BufferView {
+	return BufferView{
+		buffer:   buffer,
+		location: location,
+		width:    width,
+		height:   height,
+		cursor:   Position{row: 0, col: 0},
+	}
+}
